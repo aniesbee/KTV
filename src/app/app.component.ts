188de996b60adc75fb444cc06fbad6d1615fe7c6
Aniedi Etim <aniedibassseyetim@gmail.com>
2017-12-23 15:26:10 +0100

    Test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @Component({
   templateUrl: 'app.html'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpModule } from "@angular/http";
+import {HttpClientModule, HttpClient} from '@angular/common/http'
 import { MyApp } from './app.component';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import {VideoPlayer} from '@ionic-native/video-player'
 
 
 @NgModule({
@@ -13,7 +16,7 @@ import { MyApp } from './app.component';
     
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, HttpModule, HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +27,9 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider,
+    VideoPlayer
   ]
 })
 export class AppModule {}
