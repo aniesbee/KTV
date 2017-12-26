1d1f5dbf6d9f642c0aa8dc0fda61e7dd03915279
Aniedi Etim <aniedibassseyetim@gmail.com>
2017-12-26 09:26:52 +0100

    ktv test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from "@angular/http";
-import {HttpClientModule, HttpClient} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http'
 import { MyApp } from './app.component';
 import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 import {VideoPlayer} from '@ionic-native/video-player'
